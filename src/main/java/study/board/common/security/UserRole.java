b71a5f60a34d68f33f38388165a80df8d1bc96c5
PLT134340 <songwoo1015@gmail.com>
2023-08-21 16:43:28 +0900

    rename: #73 - enum 이름 수정
    UserPermission -> UserRole

--- src/main/java/study/board/common/security/UserRole.java
+++ src/main/java/study/board/common/security/UserRole.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum UserPermission {
+public enum UserRole {
     ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
 
     private final String value;
